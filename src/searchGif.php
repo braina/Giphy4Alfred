fc1b50a2470cf15eac84e65fe34be89fb36912f7
braina <nh@NHMBP.local>
2015-05-07 06:41:13 +0900

    simplify code

--- src/searchGif.php
+++ src/searchGif.php
@@ -4,15 +4,11 @@ require_once('workflows.php');
 $wf = new Workflows();
 $searchWord = $argv[1];
 
-
-
-define("REQUEST_URL", "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC");
-$url = REQUEST_URL."&q=".urlencode( $searchWord );
+$randomURL= "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC";
+$url = $randomURL."&tag=".urlencode( $searchWord );
 
 $json = json_decode( $wf -> request($url));
-$num = rand(0,count($json->data));
-$url = $json->data[$num]->images->original -> url;
-
+$url = $json->data->image_url;
 
 if(count($json->data)==0)
     $wf -> result (time(),$url, "no result","$url", 'icon.png');
@@ -24,3 +20,6 @@ echo $wf->toxml();
 
 
 ?>
+
+
+
